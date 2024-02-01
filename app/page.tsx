ba6e860ea464a830693357cdd36534763b6218fb
OferEzrachi <ofer@ironalgo.cc>
2024-02-01 14:25:40 +0200

    adding gender support

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,9 @@ import Simulator from '@/lib/components/simulator/simulator';
 export default function Home() {
   
   return (
-    <WebsiteScreen>
         <div className="center padding-2">
           <h1>Welcom to the pension calculator</h1>
           <Simulator/>
         </div>
-    </WebsiteScreen>
   )
 }
--- lib/components/simulator/simulator_input/basic-input.tsx
+++ lib/components/simulator/simulator_input/basic-input.tsx
@@ -12,22 +12,28 @@ export default function BasicInput(props: any) {
         var salary_element = (document.getElementById("salary") as HTMLInputElement);
         setSalary(salary_element?.value);
     }
-    // function change_is_male() {
-    //     var is_male_element = (document.getElementsByName("mm") as NodeListOf<HTMLElement>);
-    //     setIsMale(is_male_element?.value);
-    // }
+    function change_is_male(e:any) {
+        setIsMale(e.currentTarget.value=="1");
+    }
     return (
         <div>
+            <h3>Basic Information</h3>
+            <div className="slider-container">
+            <label>Age: </label>
             <input type="number" id ="age" onChange={change_age} value={age}/>
-            <br/>
+            <label>Salary: </label>
             <input type="number" id ="salary" onChange={change_slary} value={salary}/>
             <br/>
-            {/* <div>
-            <input type="radio" id ="is_male" onChange={change_is_male} name="mm" value={is_male?"checked":""}/>
-            <input type="radio" id ="is_female" onChange={change_is_male} name="mm" value={is_male?"checked":""}/>
-            </div> */}
+            <div>
+            <input type="radio" name="gender" value={1} onChange={change_is_male}/>
+              Male
+              <input type="radio" name="gender" value={0} onChange={change_is_male} />
+              Female
+            </div>
             
         </div>
+        </div>
+        
     );
 
 }
