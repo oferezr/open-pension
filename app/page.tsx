7cd44f1a75a3a8f9dc3dd76a36d31ec908d75bed
OferEzrachi <ofer@ironalgo.cc>
2024-02-08 13:47:37 +0200

    working grid

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@
 import LandingExplanation from '@/lib/components/landing/landing-explanation';
 import LandingInput from '@/lib/components/landing/landing-input';
 export default function Home() {
-  const grid_columns = [1, 2, 3,4,5,6,7];
-  const grid_rows = [1,2,3,4,5,6]
+  const grid_columns = [1, 2, 3,4,5,6,7,8];
+  const grid_rows = [1,2,3,4,5,6,7,8,9,10,11,12]
 
   return (
     <div id="wrapper" dir="rtl">
@@ -17,15 +17,13 @@ export default function Home() {
         </div>
       </div>
       <div className='backgound-tilt-grid'>
-        <div className="parent">
           {grid_rows.map(r=>
             <div className={"div"+r} key={"r"+r}>
             {grid_columns.map(n=><svg key={"r"+r+n} width="172" height="100" viewBox="0 0 172 99" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M1.99648 48.2609L84.005 1.39891L169.459 49.2532L84.9971 96.9002L1.99648 48.2609Z" fill="#9EDA82" stroke="#B0F191" stroke-width="2" />
+              <path d="M1.99648 48.2609L84.005 1.39891L169.459 49.2532L84.9971 96.9002L1.99648 48.2609Z" fill="#9EDA82" stroke="#b0f19173" stroke-width="2" />
             </svg>)}
           </div>
             )}
-        </div>
       </div>
 
     </div>
