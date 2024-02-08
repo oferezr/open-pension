9d8e13c37db5e9113cecb630a340bc62631c0a76
OferEzrachi <ofer@ironalgo.cc>
2024-02-08 13:28:41 +0200

    working overlaping grid

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@
 import LandingExplanation from '@/lib/components/landing/landing-explanation';
 import LandingInput from '@/lib/components/landing/landing-input';
 export default function Home() {
+  const grid_columns = [1, 2, 3,4,5,6,7];
+  const grid_rows = [1,2,3,4,5,6]
 
   return (
     <div id="wrapper" dir="rtl">
@@ -15,74 +17,17 @@ export default function Home() {
         </div>
       </div>
       <div className='backgound-tilt-grid'>
-      <div className="parent">
-<div className="div1"> </div>
-<div className="div2"> </div>
-<div className="div3"> </div>
-<div className="div4"> </div>
-<div className="div5"> </div>
-<div className="div6"> </div>
-<div className="div7"> </div>
-<div className="div8"> </div>
-<div className="div9"> </div>
-<div className="div10"> </div>
-<div className="div11"> </div>
-<div className="div12"> </div>
-<div className="div13"> </div>
-<div className="div14"> </div>
-<div className="div15"> </div>
-<div className="div16"> </div>
-<div className="div17"> </div>
-<div className="div18"> </div>
-<div className="div19"> </div>
-<div className="div20"> </div>
-<div className="div21"> </div>
-<div className="div22"> </div>
-<div className="div23"> </div>
-<div className="div24"> </div>
-<div className="div25"> </div>
-<div className="div26"> </div>
-<div className="div27"> </div>
-<div className="div28"> </div>
-<div className="div29"> </div>
-<div className="div30"> </div>
-<div className="div31"> </div>
-<div className="div32"> </div>
-<div className="div33"> </div>
-<div className="div34"> </div>
-<div className="div35"> </div>
-<div className="div36"> </div>
-<div className="div37"> </div>
-<div className="div38"> </div>
-<div className="div39"> </div>
-<div className="div40"> </div>
-<div className="div41"> </div>
-<div className="div42"> </div>
-<div className="div43"> </div>
-<div className="div44"> </div>
-<div className="div45"> </div>
-<div className="div46"> </div>
-<div className="div47"> </div>
-<div className="div48"> </div>
-<div className="div49"> </div>
-<div className="div50"> </div>
-<div className="div51"> </div>
-<div className="div52"> </div>
-<div className="div53"> </div>
-<div className="div54"> </div>
-<div className="div55"> </div>
-<div className="div56"> </div>
-<div className="div57"> </div>
-<div className="div58"> </div>
-<div className="div59"> </div>
-<div className="div60"> </div>
-<div className="div61"> </div>
-<div className="div62"> </div>
-<div className="div63"> </div>
-<div className="div64"> </div>
-</div>
-
+        <div className="parent">
+          {grid_rows.map(r=>
+            <div className={"div"+r} key={"r"+r}>
+            {grid_columns.map(n=><svg key={"r"+r+n} width="172" height="100" viewBox="0 0 172 99" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M1.99648 48.2609L84.005 1.39891L169.459 49.2532L84.9971 96.9002L1.99648 48.2609Z" fill="#9EDA82" stroke="#B0F191" stroke-width="2" />
+            </svg>)}
+          </div>
+            )}
+        </div>
       </div>
+
     </div>
   )
 }
