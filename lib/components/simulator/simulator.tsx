fd0c8f05038e31c1d58be67bdadf6531a8bcf024
OferEzrachi <ofer@ironalgo.cc>
2024-02-14 12:37:32 +0200

    sec atmpt

--- lib/components/simulator/simulator.tsx
+++ lib/components/simulator/simulator.tsx
@@ -11,15 +11,15 @@ import PastResult from './simulator_output/past_result';
 
 const dataApiEndpoint = "https://data.gov.il/api/3/action/datastore_search?resource_id=6d47d6b5-cb08-488b-b333-f1e717b1e1bd";
 
-export interface ParentCompany{
-    id:number;
-    name:string;
+export interface ParentCompany {
+    id: number;
+    name: string;
 }
 
 interface RawFundsDataRow {
     FUND_ID: string;
     FUND_NAME: string;
-    PARENT_COMPANY_ID:number;
+    PARENT_COMPANY_ID: number;
     PARENT_COMPANY_NAME: string;
     AVG_ANNUAL_MANAGEMENT_FEE: number;
     AVG_DEPOSIT_FEE: number;
@@ -40,7 +40,7 @@ interface FundsData {
 }
 
 interface UserValDict {
-    numberInputSal:  number;
+    numberInputSal: number;
     numberInputAge: number;
     isMale: boolean;
 }
@@ -60,48 +60,51 @@ async function getFunds(): Promise<FundsData | null> {
     return null;
 }
 
-function getSearchSTD(stds:number[], interest_slider:number){
+function getSearchSTD(stds: number[], interest_slider: number) {
     const max_std = Math.max(...stds);
     const min_std = Math.min(...stds);
-    const std_to_serach = min_std + interest_slider*(max_std- min_std);
+    const std_to_serach = min_std + interest_slider * (max_std - min_std);
     return std_to_serach
- }
-
-function getFund(funds:FundsData|null, parent_company:number, interest_slider:number ):RawFundsDataRow[]{
-    if (!funds){
-        return []
-    }
-    const company_funds = funds.result.filter(f=> f.PARENT_COMPANY_ID==parent_company).sort((a,b)=>a.STANDARD_DEVIATION<=b.STANDARD_DEVIATION? -1:1);
-    const stds = company_funds.map(f=>f.STANDARD_DEVIATION);
-    
-    const std_to_serach = getSearchSTD(stds, interest_slider);
-    console.log("Current std: "+std_to_serach);
-    return company_funds.filter(f=>f.STANDARD_DEVIATION>=std_to_serach);
 }
 
-function getUniqueParents(funds:FundsData):ParentCompany[] |null{
-    if(!funds){
+function getFund(funds: FundsData | null, parent_company: number, interest_slider: number): RawFundsDataRow[] {
+    if (!funds) {
+        return []
+    }
+    const company_funds = funds.result.filter(f => f.PARENT_COMPANY_ID == parent_company).sort((a, b) => a.STANDARD_DEVIATION <= b.STANDARD_DEVIATION ? -1 : 1);
+    const stds = company_funds.map(f => f.STANDARD_DEVIATION);
+
+    const std_to_serach = getSearchSTD(stds, interest_slider);
+    console.log("Current std: " + std_to_serach);
+    return company_funds.filter(f => f.STANDARD_DEVIATION >= std_to_serach);
+}
+
+function getUniqueParents(funds: FundsData): ParentCompany[] | null {
+    if (!funds) {
         return null
     }
-    const uniques:number[] = [];
-    const uniques_companies:ParentCompany[] = [];
-    funds.result.map(val=>{
-        if(uniques.indexOf(val.PARENT_COMPANY_ID)==-1){
+    const uniques: number[] = [];
+    const uniques_companies: ParentCompany[] = [];
+    funds.result.map(val => {
+        if (uniques.indexOf(val.PARENT_COMPANY_ID) == -1) {
             uniques.push(val.PARENT_COMPANY_ID);
             const id = val.PARENT_COMPANY_ID
             const name = val.PARENT_COMPANY_NAME;
-            uniques_companies.push({id, name} as ParentCompany)
+            uniques_companies.push({ id, name } as ParentCompany)
         }
     })
     return uniques_companies
 }
 
 export default function Simulator() {
-    // getting user related values from landng 
-    var sessionString = sessionStorage.getItem('userValDict');
+    var sessionString:null|string = null;
+    if (window!=undefined && window.sessionStorage) {
+        // getting user related values from landng 
+        var sessionString = sessionStorage.getItem('userValDict');
+    }
     // TODO : import UserValDict interface to creat ONE SOURCE OF TRUTH.
     var userValDict: UserValDict = sessionString ? JSON.parse(sessionString) :
-     { numberInputAge: '',  numberInputSal:  '', isMale:''};
+        { numberInputAge: '', numberInputSal: '', isMale: '' };
     console.log(userValDict);
     const [yearly_slider, setYearlySlider] = useState(0.5);
     const [interest_slider, setInterestSlider] = useState(0.5);
@@ -112,9 +115,9 @@ export default function Simulator() {
     const [parent_company, setParentCompany] = useState(163);
     const monthly = monthly_income_by_by_sliders(salary, age, interest_slider, yearly_slider, is_male);
     const total_savings = saving_by_by_sliders(salary, age, interest_slider, yearly_slider, is_male);
-    const company_funds = getFund(funds,parent_company,yearly_slider);
-    
-   
+    const company_funds = getFund(funds, parent_company, yearly_slider);
+
+
 
 
     useEffect(() => {
@@ -126,15 +129,15 @@ export default function Simulator() {
     if (!funds) {
         return <div>loading...</div>
     }
-    
+
     return (
         <div>
             <BasicInput age={[age, setAge]} salary={[salary, setSalary]} is_male={[is_male, setIsMale]} />
-            <FundDropDown function={[parent_company, setParentCompany]} funds ={getUniqueParents(funds)}/>
+            <FundDropDown function={[parent_company, setParentCompany]} funds={getUniqueParents(funds)} />
             <Slider function={[interest_slider, setInterestSlider]} title="Interest" left="Low Deposite interest" right="Low Saving interest" />
             <Slider function={[yearly_slider, setYearlySlider]} title="Yearly" left="Security" right="Yearly" />
-            <PastResult fund={company_funds[0]}/>
-            <FutureResult monthly={monthly} total_savings={total_savings}/>
+            <PastResult fund={company_funds[0]} />
+            <FutureResult monthly={monthly} total_savings={total_savings} />
         </div>
 
     );
