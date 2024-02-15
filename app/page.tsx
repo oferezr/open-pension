af0b5cd68332164fed73b77b03703502c62edafc
Ofer Ezrachi <65119815+oferezr@users.noreply.github.com>
2024-02-15 14:17:53 +0200

    Ofer/feat/route component (#6)
    * adding basic components of the route- next- design
    
    * setting sizes
    
    * working grid
    
    * refactor
    
    * adding icon
    
    * just designing the slider itself left
    
    * a bit more font
    
    * slider ready
    
    * tring to add the bars
    
    ---------
    
    Co-authored-by: OferEzrachi <ofer@ironalgo.cc>

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,21 @@
 import LandingExplanation from '@/lib/components/landing/landing-explanation';
 import LandingInput from '@/lib/components/landing/landing-input';
 
+function Comp(props: any) {
+  return (
+    <div className='tile'>
+      <svg width="141" height="72" viewBox="0 0 141 72" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M140.29 36.2005L70.1896 71.8505L0.0996094 36.2005L70.1896 0.560547L140.29 36.2005Z" stroke="#b0f19173"/>
+      </svg>
+
+    </div>
+
+  )
+}
 
 export default function Home() {
-  const grid_columns = [1];
-  const grid_rows = [1,2,3,4,5,6]
-  const grid_item_width = 182;
-  for(let i=grid_item_width ; i<1920- 2*grid_item_width; i+=grid_item_width){
-    grid_columns.push(i)
-  }
+  const grid_columns = [1, 2, 3, 4, 5, 6, 7, 8];
+  const grid_rows = [1, 2, 3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
 
   return (
     <div id="wrapper" dir="rtl">
@@ -23,15 +30,13 @@ export default function Home() {
         </div>
       </div>
       <div className='backgound-tilt-grid'>
-          {grid_rows.map(r=>
-            <div className={"div"+r+" row"} key={"r"+r}>
-            {grid_columns.map(n=>
-            <svg key={"r"+r+n} width={grid_item_width} height="111" viewBox="0 0 182 111" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M1.93436 55.0096L91.9928 1.17027L180.094 55.0094L91.9931 109.828L1.93436 55.0096Z" stroke="#b0f19173" strokeWidth="2"/>
-            </svg>
+        {grid_rows.map(r =>
+          <div className={"div" + r + " row"} key={"r" + r}>
+            {grid_columns.map(n =>
+              <Comp r={r} c={n} key={r + "_" + n} />
             )}
           </div>
-            )}
+        )}
       </div>
 
     </div>
--- app/simulator/page.tsx
+++ app/simulator/page.tsx
@@ -1,17 +1,19 @@
 /// <reference lib="dom" />
-
+'use client';
 import FundInput from "@/lib/components/simulator/simulator_input/fund-input";
 import PersonalInput from "@/lib/components/simulator/simulator_input/personal-input";
 import RouteInput from "@/lib/components/simulator/simulator_input/route-input";
+import { useState } from "react";
 
 export default function Home() {
+  const [route, setRoute] = useState(0.5);
   
   return (
         <div dir='rtl'>
-          <h1>הסדנא</h1>
+          <h1>פנסיה פתוחה</h1>
             <PersonalInput/>
          
-            <RouteInput/>
+            <RouteInput function={[route, setRoute]}/>
           
             <FundInput/>
           {/* <Comparsion/> */}
