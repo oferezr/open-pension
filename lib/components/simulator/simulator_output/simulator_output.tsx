cb70068c98ce111a28c1599fb5f56a788002abc1
Neel Bram <neelbram1@gmail.com>
2024-03-13 09:19:20 +0200

    last ui changes

--- lib/components/simulator/simulator_output/simulator_output.tsx
+++ lib/components/simulator/simulator_output/simulator_output.tsx
@@ -1,5 +1,9 @@
 import { Tile } from "../../tile";
 import ComparisonTab from "../comparsion-tab";
+// 'use client'
+// 'use state'
+import { useState } from "react";
+import{useEffect} from "react";
 
 export default function SimulatorOutput(props: any) {
     const income = props.monthly;
@@ -12,6 +16,26 @@ export default function SimulatorOutput(props: any) {
     const grid_columns = [1, 2, 3, 4, 5, 6, 7, 8];
     const grid_rows = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
     const pred_elems_margin_bottom = tabFilled?"6%":"3%";
+    const [continue3Clicked, setcontinue3Clicked] = props.continue3Clicked;
+    // const [distanceFromBottom, setDistanceFromBottom] = useState(0);
+
+    const openComaprisons = () => {
+        if (continue3Clicked === true){
+            console.log("continue3Clicked"+continue3Clicked)
+        }
+    };
+
+    // const OtherComponent = ({ continue3Clicked }) => {
+    //     const [distanceFromBottom, setDistanceFromBottom] = useState(50);
+    
+        // useEffect(() => {
+        //     if (continue3Clicked) {
+        //         // Change the distance from bottom here when isButtonClicked becomes true
+        //         setDistanceFromBottom(prevDistance => prevDistance - 20); // Move the object up by 20px
+        //     }
+        // }, [continue3Clicked]);
+    
+
     return (
         <div style={{opacity:active?1:0.5}}>
             <div className="output-tiles">
@@ -25,7 +49,9 @@ export default function SimulatorOutput(props: any) {
                     )}
                 </div>
             </div>
-            <div className="pred-elements">
+            {/* style={{ position: 'fixed', bottom: distanceFromBottom, left: 20 }} */}
+            <div className="pred-elements"
+           >
                 <div id="monthly" className="elm">
                     <div className="pred-icon">
                     <svg width="38" height="31" viewBox="0 0 38 31" fill="none" xmlns="http://www.w3.org/2000/svg">
